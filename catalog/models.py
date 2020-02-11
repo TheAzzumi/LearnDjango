5ead21cd9abfdcd1752046c27d41d726bcac9ed0
Azzumi <egorkalass.d@mail.ru>
2020-02-11 12:21:08 +0400

    Тесты для отображений

--- catalog/models.py
+++ catalog/models.py
@@ -4,6 +4,7 @@ import uuid
 from django.contrib.auth.models import User
 from datetime import date
 
+
 class Genre(models.Model):
     name = models.CharField(max_length=100, help_text='Введите жанр книги')
 
@@ -13,6 +14,7 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Language(models.Model):
     name = models.CharField(max_length=100, help_text='Выберите язык вашей книги')
@@ -24,11 +26,13 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text='Введите краткое описание книги')
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Выберите жанр для этой кнмги')
     language = models.ManyToManyField(Language, help_text='Выберите язык вашей книги')
 
@@ -49,7 +53,8 @@ class Book(models.Model):
 
 
 class BookInstance(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Уникальный идентификатор для книги в бииблиотеке')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text='Уникальный идентификатор для книги в бииблиотеке')
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
@@ -79,6 +84,7 @@ class BookInstance(models.Model):
             return True
         return False
 
+
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -94,6 +100,3 @@ class Author(models.Model):
 
     def __str__(self):
         return '{0} {1}'.format(self.first_name, self.last_name)
-
-
-
